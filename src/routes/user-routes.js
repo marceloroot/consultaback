572b448737f818a29e52e9fe0f24328818f6d457
marceloroot <marcelo.lima.gomes.23@gmail.com>
2023-07-05 20:47:04 -0300

    Update user-routes.js

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const authService = require('../services/auth-services');
 const controller = require('../controllers/user-controller');
 
-router.post('/',authService.authorize,controller.store);
+router.post('/',controller.store);
 router.post('/authenticate',controller.authenticate);
 router.post('/decode',controller.decoude);
 module.exports =router;
